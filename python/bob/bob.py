514aac61cb155f81af81ec2b5f757b0a170c35ff
Chauntel Kellar <chauntelk@gmail.com>
2023-05-28 07:46:24 -0700

    bob exercise

---
+++ python/bob/bob.py
@@ -0,0 +1,15 @@
+def response(hey_bob):
+    hey_bob = hey_bob.strip()  # Remove leading/trailing whitespace
+    
+    if not hey_bob:
+        return "Fine. Be that way!"
+    
+    if hey_bob.endswith("?"):
+        if hey_bob.isupper():
+            return "Calm down, I know what I'm doing!"
+        return "Sure."
+    
+    if hey_bob.isupper():
+        return "Whoa, chill out!"
+    
+    return "Whatever."
